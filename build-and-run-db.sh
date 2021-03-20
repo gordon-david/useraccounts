eeeea034f458834c4055007c35f6bcf2b19f7c6c
david <david@localhost.localdomain>
2021-03-19 22:46:10 -0500

    updated build script

--- build-and-run-db.sh
+++ build-and-run-db.sh
@@ -1,1 +1,1 @@
-sudo rm -r db_volume/ && mkdir db_volume && sudo docker-compose rm && sudo docker-compose build && sudo docker-compose up
+sudo docker-compose rm && sudo docker-compose build && sudo docker-compose up
