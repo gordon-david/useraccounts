fb40027fa5927daec5232e5eda6814b9b627667a
David <d.phi.gordon@gmail.com>
2021-04-16 13:09:42 -0500

    fixed the invisible submit button on the login form

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -36,7 +36,7 @@
         <div class="flex items-center justify-between">
           <button
             @click="submitLogin()"
-            class="bg-blue-501 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
           >
             Submit
