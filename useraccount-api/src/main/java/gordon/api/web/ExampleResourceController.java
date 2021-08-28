f6ff83d3d0f7fe36fb7e5ef19a0bf5425504b9ae
David <d.phi.gordon@gmail.com>
2021-08-28 17:39:31 -0500

    changed example resource class name to be more consistent with similar classes

--- useraccount-api/src/main/java/gordon/api/web/ExampleResourceController.java
+++ useraccount-api/src/main/java/gordon/api/web/ExampleResourceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class ExampleResource {
+public class ExampleResourceController {
 
     // accessible publicly
     @GetMapping("/hometestresource")
